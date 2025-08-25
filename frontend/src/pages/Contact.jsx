383b75e3e4b7bc2d3cc71828730991a87baa428e
anindaic3842 <anindaic3842@gmail.com>
2025-08-24 20:10:21 -0400

    Updated the API for contact - added access to environment variables

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
     setErrors({})
     
     try {
-      const response = await fetch('/api/contact', {
+      const response = await fetch('${import.meta.env.VITE_API_URL}api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
